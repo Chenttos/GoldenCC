265a272761ceb718f67397ae049cc2216be3f737
Samue <113913909+Chenttos@users.noreply.github.com>
2026-08-19 07:24:59 -0300

    Update CCARootListController.m

--- prefs/CCARootListController.m
+++ prefs/CCARootListController.m
@@ -1,5 +1,6 @@
 #import "CCARootListController.h"
 #import <Preferences/PSSpecifier.h>
+#import <CoreFoundation/CoreFoundation.h>
 #import <spawn.h>
 #import <unistd.h>
 #import <notify.h>
@@ -7,21 +8,40 @@
 
 extern char **environ;
 
-static NSString * const kCCARootResetSpecifierKey = @"GoldenCCResetControlCenterLayout";
+static CFStringRef const kCCAPrefsDomain =
+    CFSTR("com.futur3sn0w.ccaster.preferences");
+
+static NSString * const kCCAReloadNotification =
+    @"com.futur3sn0w.ccaster/ReloadPrefs";
+
+static NSString * const kCCARootResetSpecifierKey =
+    @"GoldenCCResetControlCenterLayout";
 
 @interface CCARootListController ()
-@property (nonatomic, assign) BOOL initialEnabledValue;
 @end
 
 @implementation CCARootListController
 
 - (NSArray *)specifiers {
     if (!_specifiers) {
-        NSMutableArray *specifiers =
-            [[self loadSpecifiersFromPlistName:@"Root" target:self] mutableCopy];
 
-        // Add the reset button programmatically so no change to Root.plist is
-        // required. It appears at the bottom of the existing GoldenCC settings.
+        NSMutableArray *specifiers =
+            [[self loadSpecifiersFromPlistName:@"Root"
+                                        target:self] mutableCopy];
+
+        /*
+         * Grupo
+         */
+        PSSpecifier *group =
+            [PSSpecifier groupSpecifierWithName:@"Control Center"];
+
+        [group setProperty:
+            @"Reset GoldenCC's saved Control Center layout, sizes and duplicate modules."
+            forKey:@"footerText"];
+
+        /*
+         * Botão
+         */
         PSSpecifier *resetButton =
             [PSSpecifier preferenceSpecifierNamed:@"Reset Control Center Layout"
                                             target:self
@@ -31,18 +51,13 @@ static NSString * const kCCARootResetSpecifierKey = @"GoldenCCResetControlCenter
                                               cell:PSButtonCell
                                               edit:Nil];
 
-        [resetButton setProperty:kCCARootResetSpecifierKey forKey:@"id"];
-        [resetButton setProperty:@"resetControlCenterLayout" forKey:@"action"];
-        [resetButton setProperty:@YES forKey:@"isController"];
+        [resetButton setProperty:kCCARootResetSpecifierKey
+                          forKey:@"id"];
 
-        PSSpecifier *resetGroup =
-            [PSSpecifier groupSpecifierWithName:@"Control Center"];
+        [resetButton setProperty:@"resetControlCenterLayout"
+                          forKey:@"action"];
 
-        [resetGroup setProperty:
-            @"Clears GoldenCC's saved module positions, sizes, and duplicate-module state without removing the tweak."
-            forKey:@"footerText"];
-
-        [specifiers addObject:resetGroup];
+        [specifiers addObject:group];
         [specifiers addObject:resetButton];
 
         _specifiers = [specifiers copy];
@@ -51,117 +66,185 @@ static NSString * const kCCARootResetSpecifierKey = @"GoldenCCResetControlCenter
     return _specifiers;
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-
-    for (PSSpecifier *specifier in self.specifiers) {
-        if ([[specifier propertyForKey:@"key"] isEqualToString:@"Enabled"]) {
-            self.initialEnabledValue =
-                [[self readPreferenceValue:specifier] boolValue];
-            break;
-        }
-    }
-
-    self.navigationItem.rightBarButtonItem = nil;
-}
-
-- (void)setMasterEnabled:(id)value specifier:(PSSpecifier *)specifier {
-    [self setPreferenceValue:value specifier:specifier];
-
-    BOOL needsRespring =
-        [value boolValue] != self.initialEnabledValue;
-
-    if (!needsRespring) {
-        self.navigationItem.rightBarButtonItem = nil;
-        return;
-    }
-
-    if (self.navigationItem.rightBarButtonItem) return;
-
-    UIImage *image = [UIImage systemImageNamed:@"arrow.clockwise"];
-    UIBarButtonItem *button =
-        [[UIBarButtonItem alloc] initWithImage:image
-                                        style:UIBarButtonItemStylePlain
-                                       target:self
-                                       action:@selector(respring)];
-
-    button.accessibilityLabel = @"Respring";
-    self.navigationItem.rightBarButtonItem = button;
-}
+/*
+ * ---------------------------------------------------------
+ * RESET CONTROL CENTER
+ * ---------------------------------------------------------
+ */
 
 - (void)resetControlCenterLayout {
+
     UIAlertController *alert =
-        [UIAlertController alertControllerWithTitle:@"Reset Control Center Layout?"
-                                            message:@"This will remove GoldenCC's saved module positions, sizes, and duplicate-module state. Your normal Control Center configuration will not be deleted."
-                                     preferredStyle:UIAlertControllerStyleAlert];
+        [UIAlertController
+            alertControllerWithTitle:@"Reset Control Center Layout?"
+                             message:
+        @"This will remove GoldenCC's saved positions, sizes and duplicate modules. The tweak itself will not be removed."
+                      preferredStyle:UIAlertControllerStyleAlert];
 
     UIAlertAction *cancel =
-        [UIAlertAction actionWithTitle:@"Cancel"
-                                 style:UIAlertActionStyleCancel
-                               handler:nil];
+        [UIAlertAction
+            actionWithTitle:@"Cancel"
+                      style:UIAlertActionStyleCancel
+                    handler:nil];
 
     __weak typeof(self) weakSelf = self;
 
     UIAlertAction *reset =
-        [UIAlertAction actionWithTitle:@"Reset"
-                                 style:UIAlertActionStyleDestructive
-                               handler:^(__unused UIAlertAction *action) {
+        [UIAlertAction
+            actionWithTitle:@"Reset"
+                      style:UIAlertActionStyleDestructive
+                    handler:^(__unused UIAlertAction *action) {
+
         __strong typeof(weakSelf) self = weakSelf;
         if (!self) return;
 
-        notify_post("com.futur3sn0w.ccaster/ResetControlCenterLayout");
+        /*
+         * GoldenCC salva o layout nestas três chaves.
+         *
+         * ModuleGridOrigins
+         * ModuleGridSizes
+         * COSMICDuplicateFamilies
+         */
 
-        dispatch_async(dispatch_get_main_queue(), ^{
-            UIAlertController *done =
-                [UIAlertController alertControllerWithTitle:@"GoldenCC"
-                                                    message:@"Control Center layout reset."
-                                             preferredStyle:UIAlertControllerStyleAlert];
+        CFPreferencesSetAppValue(
+            CFSTR("ModuleGridOrigins"),
+            NULL,
+            kCCAPrefsDomain
+        );
 
-            [done addAction:
-                [UIAlertAction actionWithTitle:@"OK"
-                                         style:UIAlertActionStyleDefault
-                                       handler:nil]];
+        CFPreferencesSetAppValue(
+            CFSTR("ModuleGridSizes"),
+            NULL,
+            kCCAPrefsDomain
+        );
 
-            [self presentViewController:done animated:YES completion:nil];
-        });
+        CFPreferencesSetAppValue(
+            CFSTR("COSMICDuplicateFamilies"),
+            NULL,
+            kCCAPrefsDomain
+        );
+
+        /*
+         * Garante que as alterações sejam gravadas imediatamente.
+         */
+        CFPreferencesAppSynchronize(kCCAPrefsDomain);
+
+        /*
+         * A notificação correta que o Tweak.xm realmente registra.
+         */
+        notify_post(
+            [kCCAReloadNotification UTF8String]
+        );
+
+        /*
+         * Recarrega SpringBoard/Control Center.
+         *
+         * Isso é importante porque o Tweak.xm carrega os dicionários
+         * de layout no %ctor.
+         */
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                (int64_t)(0.15 * NSEC_PER_SEC)
+            ),
+            dispatch_get_main_queue(),
+            ^{
+
+                const char *sbreloadPath = NULL;
+
+                if (access("/var/jb/usr/bin/sbreload", X_OK) == 0) {
+                    sbreloadPath = "/var/jb/usr/bin/sbreload";
+                }
+                else if (access("/usr/bin/sbreload", X_OK) == 0) {
+                    sbreloadPath = "/usr/bin/sbreload";
+                }
+
+                if (sbreloadPath) {
+
+                    const char *args[] = {
+                        sbreloadPath,
+                        NULL
+                    };
+
+                    pid_t pid = 0;
+
+                    int result =
+                        posix_spawn(
+                            &pid,
+                            sbreloadPath,
+                            NULL,
+                            NULL,
+                            (char *const *)args,
+                            environ
+                        );
+
+                    /*
+                     * Se o sbreload foi iniciado, não precisamos
+                     * mostrar outro alerta.
+                     */
+                    if (result == 0) {
+                        return;
+                    }
+                }
+
+                /*
+                 * Caso sbreload não exista ou falhe,
+                 * informa o usuário.
+                 */
+                UIAlertController *failed =
+                    [UIAlertController
+                        alertControllerWithTitle:@"GoldenCC"
+                                         message:
+                    @"The layout was reset, but SpringBoard could not be reloaded automatically. Please respring manually."
+                                  preferredStyle:UIAlertControllerStyleAlert];
+
+                [failed
+                    addAction:
+                        [UIAlertAction
+                            actionWithTitle:@"OK"
+                                      style:UIAlertActionStyleDefault
+                                    handler:nil]];
+
+                [self presentViewController:failed
+                                    animated:YES
+                                  completion:nil];
+            }
+        );
     }];
 
     [alert addAction:cancel];
     [alert addAction:reset];
 
-    [self presentViewController:alert animated:YES completion:nil];
+    [self presentViewController:alert
+                       animated:YES
+                     completion:nil];
 }
 
-// Fallback for PreferenceLoader versions that don't invoke the PSButtonCell
-// "action" property consistently.
+/*
+ * Cool tweak made by me:3
+ * Below is some configurations for PSButtonCell.
+ */
 - (void)tableView:(UITableView *)tableView
 didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    PSSpecifier *specifier = [self specifierAtIndexPath:indexPath];
 
-    NSString *identifier = [specifier propertyForKey:@"id"];
-    if ([identifier isEqualToString:kCCARootResetSpecifierKey]) {
-        [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    PSSpecifier *specifier =
+        [self specifierAtIndexPath:indexPath];
+
+    NSString *identifier =
+        [specifier propertyForKey:@"id"];
+
+    if ([identifier
+            isEqualToString:kCCARootResetSpecifierKey]) {
+
+        [tableView deselectRowAtIndexPath:indexPath
+                                 animated:YES];
+
         [self resetControlCenterLayout];
         return;
     }
 
-    [super tableView:tableView didSelectRowAtIndexPath:indexPath];
-}
-
-- (void)respring {
-    const char *path = access("/var/jb/usr/bin/sbreload", X_OK) == 0
-        ? "/var/jb/usr/bin/sbreload"
-        : "/usr/bin/sbreload";
-
-    const char *arguments[] = { path, NULL };
-
-    pid_t pid = 0;
-    posix_spawn(&pid,
-                path,
-                NULL,
-                NULL,
-                (char *const *)arguments,
-                environ);
+    [super tableView:tableView
+didSelectRowAtIndexPath:indexPath];
 }
 
 @end
